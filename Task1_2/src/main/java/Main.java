9316c3cd37c7bc483258d71f449c99486092a7e5
TheOriginalTiger <vvp.vovarulit@gmail.com>
2020-10-16 20:12:42 +0700

    changed the search func in Searcher so it doesnt create tons of useless Strings. Now it performes a search in byte array using z-func algo. TODO: fix tests

--- Task1_2/src/main/java/Main.java
+++ Task1_2/src/main/java/Main.java
@@ -8,12 +8,65 @@ import java.io.FileNotFoundException;
 class Searcher
 {
 
+
+    private static int min(int a, int b)
+    {
+        return ( a < b ? a : b);
+    }
+
+    //So as it is too expensive to create Strings in while loop and they are also immutable
+    //we need to write our own searcher for byte-array. Z-func sounds like a good idea
+    private static int searchInByteArray(byte[] array, String substring,char[] finalArray,int[] z)
+    {
+        int len  = array.length + substring.length() + 2;
+
+        // preparing our array for z-func calculation
+        // [SubString...0...array]
+        for (int i = 0; i < len - 1; i++)
+        {
+            z[i] = 0 ; // also clearing z from the last call of the func , as we keep it common for all calls
+            if (i < substring.length())
+            {
+                finalArray[i] = substring.charAt(i);
+            } else if (i == substring.length())
+            {
+                finalArray[i] = 127; // kind of a special symbol
+            } else
+            {
+                finalArray[i] = (char) array[i - substring.length() - 1 ];
+            }
+        }
+        finalArray[len - 1] = 0;
+        //now calc z-func
+        int r = 0 ;
+        int l = 0 ;
+        for(int i = 1 ; i < len ; i++)
+        {
+            if (i <= r)
+            {
+                z[i] = min(r - i + 1, z[i-l]);
+            }
+            while (i+z[i] < len && finalArray[z[i]] == finalArray[i+z[i]])
+                    z[i]++;
+            if (i+z[i]-1 > r)
+            {
+                l = i;
+                r = i+z[i]-1;
+            }
+            if (z[i] == substring.length())
+                return i - substring.length() - 1;
+        }
+        return -1 ;
+    }
+
+
     /**
-     * Method for searching a substring in the file. it opens a Stream to the file and reads data to the buffer which is 2*len(Substring)
-     * after that it searches for substring in buffer and either stops it and return
-     * @param subString the String you will be looking for
-     * @param stream is the opened
-     * @return the result is either a non-negative number- index of the first substring occurrence or -1 if there is none
+     * Method for searching a substring in the file. Reads data to the buffer which is 2*len(Substring)
+     * after that it searches for a substring in buffer and either finds it in there and returns index of the first occurrence
+     * or keeps reading until it finds subString or the file will end
+     * @param subString the String to search for
+     * @param stream is the opened stream to read the string from. Can be acquired from any inheritance of abstract InputStream
+     * @return the result is either a non-negative number-index of the first substring occurrence or -1 if there is none
      * @throws IOException can throw IOException if the standart lib will disagree with you for some reason:P
      */
     static int search(String subString, InputStream stream) throws IOException
@@ -27,35 +80,35 @@ class Searcher
                 return result;
 
             byte[] buffer = new byte[len * 2];
-            int readingResult = 0;
-            readingResult = stream.read(buffer, 0, len * 2);
 
-            str = new String(buffer, StandardCharsets.UTF_8);
-            result = str.indexOf(subString);
+            if (stream.read(buffer, 0, len * 2) == -1 )
+            {
+                return -1;
+            }
+            //these 2 are for z-func calculation. It's not wise to allocate them every time we calc it, as the
+            // buff and substring lengths are constant
+            char[] finalArray = new char[subString.length() + buffer.length + 2 ];
+            int[] z = new int[subString.length() + buffer.length + 2 ];
+            result = searchInByteArray(buffer, subString, finalArray, z);
 
             if (result != -1)
                 return result;
 
             int offset = len;
             System.arraycopy(buffer, len, buffer, 0, len);
-            while ((readingResult = stream.read(buffer, len, len)) != -1)
+            while ( stream.read(buffer, len, len) != -1)
             {
-                str = new String(buffer, StandardCharsets.UTF_8);
-                result = str.indexOf(subString);
+                result = searchInByteArray(buffer, subString, finalArray, z);
                 if (result != -1)
                     return result + offset;
                 System.arraycopy(buffer, len, buffer, 0, len);
                 offset += len;
             }
             return result;
-        }
-        catch (FileNotFoundException e)
+        } catch (FileNotFoundException e)
         {
             System.out.println(e.getMessage());
             return result;
-        }
-        finally {
-            stream.close();
         }
 
     }
@@ -63,13 +116,17 @@ class Searcher
 public class Main {
 
     public static void main(String[] args) throws IOException, FileNotFoundException {
-        String ss = "e";//aba
+        FileInputStream stream = null;
+        String ss = "l";//aba
         Searcher srch = new Searcher();
-        String path ; // write down path to your input file here
-        File file = new File(path);
-        FileInputStream stream = new FileInputStream(file);
-        int result = srch.search(ss,stream);
-        System.out.println(result);
-
+        String path = "C:\\Users\\vvpvo\\Java Projects\\OOP\\Task1_2\\src\\main\\java\\input.txt"; // write down path to your input file here
+        try {
+            File file = new File(path);
+            stream = new FileInputStream(file);
+            int result = srch.search(ss, stream);
+            System.out.println(result);
+        } finally {
+            stream.close(); // after thinking it over, decided to let the user close his own InputStream himself :p
+        }
     }
 }
